37cb66a2dd3f28a5bac11cbeee51d76db5737f40
João Hugo Leda <joaohugoleda2@gmail.com>
2022-03-14 22:55:05 -0300

    Sidebar

--- dashgo/src/components/Sidebar.tsx
+++ dashgo/src/components/Sidebar.tsx
@@ -14,11 +14,11 @@ export function Sidebar() {
             GERAL
           </Text>
           <Stack spacing="4" mt="8" align="stretch">
-            <Link display="flex" align="center">
+            <Link display="flex" alignItems="center">
               <Icon as={RiDashboardLine} fontSize="20"/>
               <Text fontWeight="medium" ml="4">Dashboard</Text>
             </Link>
-            <Link display="flex" align="center">
+            <Link display="flex" alignItems="center">
               <Icon as={RiContactsLine} fontSize="20"/>
               <Text fontWeight="medium" ml="4">Usuários</Text>
             </Link>
@@ -33,11 +33,11 @@ export function Sidebar() {
             AUTOMAÇÃO
           </Text>
           <Stack spacing="4" mt="8" align="stretch">
-            <Link display="flex" align="center">
+            <Link display="flex" alignItems="center">
               <Icon as={RiInputMethodLine} fontSize="20"/>
               <Text fontWeight="medium" ml="4">Formulários</Text>
             </Link>
-            <Link display="flex" align="center">
+            <Link display="flex" alignItems="center">
               <Icon as={RiGitMergeLine} fontSize="20"/>
               <Text fontWeight="medium" ml="4">Automação</Text>
             </Link>
