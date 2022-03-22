a5966b2a44e2c08752d45023b476c012875d98e1
João Hugo Leda <joaohugoleda2@gmail.com>
2022-03-22 20:02:38 -0300

    .

--- dashgo/src/pages/index.tsx
+++ dashgo/src/pages/index.tsx
@@ -1,7 +1,19 @@
 import { Flex, Button, Stack } from '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { Input } from '../components/form/Input'
 
+type SignInFormData = {
+  email: string,
+  password: string
+}
+
 export default function SignIn() {
+  const { register, handleSubmit, formState, errors } = useForm()
+
+  const handleSign: SubmitHandler<SignInFormData> = (values, event) => {
+
+  }
+
   return (
     <Flex 
       w="100vw" 
@@ -17,11 +29,12 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDirection="column"
+        onSubmit={handleSubmit(handleSign)}
       >
 
         <Stack spacing="4">
-          <Input name="email" label="E-mail" type="email" />
-          <Input name="password" label="Senha" type="password" />              
+          <Input name="email" label="E-mail" type="email" ref={register} />
+          <Input name="password" label="Senha" type="password" ref={register} />              
         </Stack>
 
         <Button 
